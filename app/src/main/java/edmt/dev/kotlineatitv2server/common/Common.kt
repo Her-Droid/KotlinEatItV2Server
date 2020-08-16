50e4f31b58c37bd7e084a77d7bbe2843c1d6b145
Russi Hersiano <68075881+ruhc38@users.noreply.github.com>
2020-08-17 05:52:43 +0700

    Initial commit

---
+++ app/src/main/java/edmt/dev/kotlineatitv2server/common/Common.kt
@@ -0,0 +1,8 @@
+package edmt.dev.kotlineatitv2server.common
+
+import edmt.dev.kotlineatitv2server.model.ServerUserModel
+
+object Common {
+    val SERVER_REF = "Server"
+    var currentServerUser: ServerUserModel? = null
+}
